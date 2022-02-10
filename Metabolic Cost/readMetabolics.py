90abb24402ad737ad868448218ba0d1096ae42e7
inseungkang <ikang620@gmail.com>
2022-02-10 09:54:01 -0500

    take data average and add subject bodymass

--- Metabolic Cost/readMetabolics.py
+++ Metabolic Cost/readMetabolics.py
@@ -1,3 +1,4 @@
+from concurrent.futures import process
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
@@ -5,22 +6,26 @@ import os
 import myFunc
 import matplotlib.pyplot as plt
 
-current_path = os.getcwd() + "/learningalgos/Metabolic Cost/Data/"
+current_path = os.getcwd() + "\\Data\\"
+# "/learningalgos/Metabolic Cost/Data/"
+
 start = 720
 end = 3420
-mass = 60
+processedData = [np.empty((2700,0)), np.empty((2700,0)), np.empty((2700,0)), np.empty((2700,0))]
 
+massPool = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20]] 
 subjectID = ["AB1", "AB2", "AB3", "AB4", "AB5"]
 sessionNum = ["2", "3", "4", "5"]
 
-processedData = [np.empty((2700,0)), np.empty((2700,0)), np.empty((2700,0)), np.empty((2700,0))]
-
-for subject in subjectID:
-    for session in sessionNum:
+for sub_idx, subject in enumerate(subjectID):
+    for sess_idx, session in enumerate(sessionNum):
+        mass = massPool[sub_idx][sess_idx]
         filename = subject + "_Metabolics_Session" + session
 
         for file in os.listdir(current_path):
             if file.startswith(filename):
+                print("Processing: " + filename)
+
                 rSpeed = int(file.split("Right")[1].split("_")[0])
                 lSpeed = int(file.split("Right")[1].split("Left")[1].split(".")[0])
 
@@ -28,7 +33,7 @@ for subject in subjectID:
                     deltaSpeed = [6, 8, 12, 14].index(lSpeed)
                 else:
                     deltaSpeed = [6, 8, 12, 14].index(rSpeed)
-                print(filename)
+
                 data = myFunc.computeMetabolics(current_path+file, mass, start, end)
                 data = np.expand_dims(data, axis=1)
                 processedData[deltaSpeed] = np.column_stack((processedData[deltaSpeed], data))
@@ -39,16 +44,11 @@ def expFunc(x, A, B, C):
 y_vec = processedData[0].flatten()
 x_vec = np.array(range(len(y_vec)))
 
+np.save("metabolicData.npy", processedData)
 # params, _  = curve_fit(expFunc, x_vec, y_vec)
 # a, b, c = params[0], params[1], params[2]
 # print(params)
 # yfit = a*np.exp(b*x_vec)+c
 
 # z = np.polyfit(x_vec, y_vec, 30)
-
-
-# plt.plot(y_vec)
-# plt.plot(z)
-# plt.show()
-
 
--- Metabolic Cost/test.py
+++ Metabolic Cost/test.py
@@ -5,79 +5,12 @@ import os
 import myFunc
 import matplotlib.pyplot as plt
 
-current_path = os.getcwd() + "/learningalgos/Metabolic Cost/Data/"
-start = 720
-end = 3420
-mass = 60
+massPool = [[1, 2, 3, 4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]] 
+mass = massPool[2][3]
+print(mass)
 
-subjectID = ["AB3"]
-sessionNum = ["2"]
-
-def computeMetabolics(filepath, mass, start_time, end_time):
-    data = pd.read_csv(filepath, header=None)
-
-    timeStamp = data.loc[:,0].str.split(":")
-    timeConv = np.zeros(len(timeStamp))
-    for idx in range(len(timeStamp)):
-        if len(timeStamp[idx]) == 2:
-            timeConv[idx] = int(timeStamp[idx][0]) * 60 + int(timeStamp[idx][1])
-        else:
-            if int(timeStamp[idx][0]) != 1:
-                timeConv[idx] = int(timeStamp[idx][0]) * 60 + int(timeStamp[idx][1])
-            else:
-                timeConv[idx] = int(timeStamp[idx][0]) * 3600 + int(timeStamp[idx][1]) * 60 + int(timeStamp[idx][2])
-
-    # print(int(timeStamp[idx][0]) * 3600 + int(timeStamp[idx][1]) * 60 + int(timeStamp[idx][2]))
-    # print(int(timeStamp[idx][0]) * 3600)
-    # print(int(timeStamp[idx][1]) * 60)
-    # print(int(timeStamp[idx][2]))
-    
-    # plt.plot(timeConv)
-    # plt.show()
-
-    timeConv = timeConv - timeConv[0]
-    print(timeConv)
-    adapStart_idx = np.abs(timeConv - start_time).argmin()
-    print(adapStart_idx)
-    if timeConv[adapStart_idx] < start_time:
-        adapStart_idx = adapStart_idx + 1
-        print(adapStart_idx)
-    adapEnd_idx = np.abs(timeConv - end_time).argmin()
-    if timeConv[adapEnd_idx] > end_time:
-        adapEnd_idx = adapStart_idx - 1
-
-    print(adapStart_idx)
-    # timeConv = timeConv[adapStart_idx:adapEnd_idx].astype(int)-start_time
-
-    # VO2 = data.iloc[adapStart_idx:adapEnd_idx,4].to_numpy()
-    # VCO2 = data.iloc[adapStart_idx:adapEnd_idx,6].to_numpy()
-    # metabolics = (0.278*VO2 + 0.075*VCO2)/mass
-
-    # newData = np.zeros(end_time-start_time)
-    # # print(metabolics.shape)
-    # # print(timeConv.shape)
-    # # print(timeConv[-1])
-    # # plt.plot(timeConv)
-    # # plt.show()
-    # for idx, xx in enumerate(metabolics):
-    #     newData[timeConv[idx]] = xx
-
-    # for idx, xx in enumerate(newData):
-    #     if xx < 0.1:
-    #         newData[idx] = newData[idx-1]
-
-    # return newData
-
-
-for subject in subjectID:
-    for session in sessionNum:
-        filename = subject + "_Metabolics_Session" + session
-
-        for file in os.listdir(current_path):
-            if file.startswith(filename):
-
-                data = computeMetabolics(current_path+file, mass, start, end)
-
-
-
-
+# data = np.load('metabolicData.npy')
+# dataMean = np.mean(data, axis=2)
+# print(dataMean.shape)
+# plt.plot(data[3])
+# plt.show()
