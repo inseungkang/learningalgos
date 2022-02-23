bee04672b8fd7650b67e1fcf70cc92fa84ddeeb9
inseungkang <ikang620@gmail.com>
2022-02-23 01:05:11 -0500

    check

--- Controller Inference/test.py
+++ Controller Inference/test.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+from matplotlib import animation
+from matplotlib.animation import FuncAnimation
+from random import randint
 
 mkr = np.load('testMarkerData.npy')
 frc = np.load('testForceData.npy')
@@ -11,24 +14,34 @@ frc = np.load('testForceData.npy')
 # LASI = mkr[2,1,:]
 # RASI = mkr[3,1,:]
 
-pelvisPosX = np.mean(mkr[0:3,0,:], axis=0)
-pelvisPosY = np.mean(mkr[0:3,1,:], axis=0)
-pelvisPosZ = np.mean(mkr[0:3,2,:], axis=0)
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.plot3D(mkr[0,0,5000:6000], mkr[0,1,5000:6000], mkr[0,2,5000:6000], 'gray')
-# plt.show()
+# Pelvis position is the mean of 4 pelvis markers
+pelvisPosX = np.mean(mkr[0:3,0,1:], axis=0)
+pelvisPosY = np.mean(mkr[0:3,1,1:], axis=0)
+pelvisPosZ = np.mean(mkr[0:3,2,1:], axis=0)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.plot3D(pelvisPosX[1000:1500],pelvisPosY[1000:1500],pelvisPosZ[1000:1500], 'blue')
+ax.plot3D(pelvisPosX[5000:6000], pelvisPosY[5000:6000], pelvisPosZ[5000:6000], 'gray')
 plt.show()
 
-# plt.plot(mkr[0,3,1:])
-# plt.plot(mkr[1,3,1:])
-# plt.plot(mkr[2,3,1:])
-# plt.plot(mkr[3,3,1:])
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.plot3D(pelvisPosX[1000:1500],pelvisPosY[1000:1500],pelvisPosZ[1000:1500], 'blue')
+# plt.show()
 
-# plt.plot(pelvisPosZ[1:])
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+# xs = []
+# ys = []
+
+# for ii, xx in enumerate(pelvisPosX[2:]):
+#     xs.append(ii)
+#     ys.append(xx)
+
+# def animate(i):
+#     ax.clear()
+#     ax.plot(xs, ys)
+
+# # Set up plot to call animate() function every 1000 milliseconds
+# ani = FuncAnimation(fig, animate, interval=1000)
 # plt.show()
